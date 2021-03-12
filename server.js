6b870cc3cb20995b5e305e31ba2430daea617506
Your NameAlex <alehr45@gmail.com>
2021-03-12 15:49:48 -0600

    created connection to server in js file

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 // const mysql = require('mysql');
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "pioneer44",
+  database: "employees",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  promptUser();
+});
 
 function promptUser() {
-    inquirer.prompt({
-            name: 'questions',
-            type: 'list',
-            message: 'What would you like to do?',
-            choices: [
-                "View All Employees",
-                "View By Department",
-                "View By Manager",
-                "View All Roles",
-                "Add Employee",
-                "Remove Employee",
-                "Update Role",
-                "Exit"
-            ]
-        });
-    };
-
-    promptUser();
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      name: "choice",
+      choices: [
+        "View All Employees",
+        "View All Employee's By Roles",
+        "View all Emplyees By Deparments",
+        "Update Employee",
+        "Add Employee",
+        "Add Role",
+        "Add Department",
+      ],
+    },
+  ]);
+}
