95647ac120420cf0a255d34b7b9ffb381cfce255
Your NameAlex <alehr45@gmail.com>
2021-03-12 17:40:32 -0600

    view all employee function works but only shows first and last name

--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,7 @@ USE employees;
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30)
- 
-);
+ );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -25,7 +24,6 @@ CREATE TABLE employee (
   role_id INT,
   FOREIGN KEY (role_id) REFERENCES role(id),
   FOREIGN KEY (manager_id) REFERENCES employee(id)
-
 );
 
 INSERT INTO department (name)
@@ -37,20 +35,20 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES 
-("Sales Lead", "100000", 1),
-("Salesperson", "65000", 1),
-("Lead Engineer", "95000", 2),
-("Software Engineer", "75000", 2),
-("Account Manager", "85000", 3),
-("Accountant", "75000", 3),
-("Legal Team Lead", "115000", 4);
+("Sales Lead", "100000", 4),
+("Salesperson", "65000", 4),
+("Lead Engineer", "95000", 1),
+("Software Engineer", "75000", 1),
+("Account Manager", "85000", 4),
+("Accountant", "75000", 4),
+("Legal Team Lead", "115000", 3);
 
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
 VALUES 
 ("Amanda", "Fulton", null, 1),
-("Stephanie", "Dickerson", null, 2),
+("Stephanie", "Dickerson", 1 , 2),
 ("Derrick", "Smith", null, 3),
-("Bently", "Callahan", 1, 4),
-("Chris", "Jackson", 4, 5),
-("Lindsay", "Holt", 1, 6),
-("Jim", "Fairfield", 2, 7);
+("Bentley", "Callahan", 3 , 4),
+("Chris", "Jackson", null, 5),
+("Lindsay", "Holt", 5, 6),
+("Jim", "Fairfield", null, 7);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // const mysql = require('mysql');
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+const table = require('console.table');
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -16,20 +17,36 @@ connection.connect(function (err) {
 });
 
 function promptUser() {
-  inquirer.prompt([
-    {
-      type: "list",
-      message: "What would you like to do?",
-      name: "choice",
-      choices: [
-        "View All Employees",
-        "View All Employee's By Roles",
-        "View all Emplyees By Deparments",
-        "Update Employee",
-        "Add Employee",
-        "Add Role",
-        "Add Department",
-      ],
-    },
-  ]);
-}
+    inquirer.prompt([
+        {
+        type: "list",
+        message: "What would you like to do?",
+        name: "choice",
+        choices: [
+                  "View All Employees?", 
+                  "View All Employees By Roles?",
+                  "View all Employees By Departments", 
+                  "Update Employee",
+                  "Add Employee",
+                  "Add Role",
+                  "Add Department"
+                ]
+        }
+    ]).then(function(val) {
+            switch (val.choice) {
+                case "View All Employees?":
+                  viewAllEmployees();
+                break;
+        
+        
+                }
+        })
+    }
+
+function viewAllEmployees() {
+    connection.query("SELECT first_name, last_name, FROM employee;", 
+    function(err, res) {
+      if (err) throw err
+      console.table(res)
+      promptUser()
+  })};
