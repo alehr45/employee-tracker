0f27898e5b28bd08ef9efc0763d575119638dd68
Your NameAlex <alehr45@gmail.com>
2021-03-12 22:52:24 -0600

    functions for roles and departments

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // const mysql = require('mysql');
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
-const table = require('console.table');
+const table = require("console.table");
+
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -14,39 +15,104 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err;
   promptUser();
+  
 });
 
 function promptUser() {
-    inquirer.prompt([
-        {
+  inquirer
+    .prompt([
+      {
         type: "list",
         message: "What would you like to do?",
         name: "choice",
         choices: [
-                  "View All Employees?", 
-                  "View All Employees By Roles?",
-                  "View all Employees By Departments", 
-                  "Update Employee",
-                  "Add Employee",
-                  "Add Role",
-                  "Add Department"
-                ]
-        }
-    ]).then(function(val) {
-            switch (val.choice) {
-                case "View All Employees?":
-                  viewAllEmployees();
-                break;
-        
-        
-                }
-        })
-    }
+          "View All Employees",
+          "View All Employees By Roles",
+          "View All Employees By Department",
+          "Update Employee",
+          "Add Employee",
+          "Add Role",
+          "Add Department",
+          "Exit",
+        ],
+      },
+    ])
+    .then(function (val) {
+      switch (val.choice) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+        case "View All Employees By Roles":
+          viewAllRoles();
+          break;
+        case "View All Employees By Department":
+          viewAllDepartments();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "Exit":
+          connection.end();
+          break;
+      }
+    });
+}
 
 function viewAllEmployees() {
-    connection.query("SELECT first_name, last_name, FROM employee;", 
-    function(err, res) {
-      if (err) throw err
-      console.table(res)
-      promptUser()
-  })};
+  connection.query(
+    "SELECT first_name, last_name FROM employee;",
+    function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      promptUser();
+    }
+  );
+}
+
+function viewAllRoles() {
+  connection.query(
+    "SELECT role.title, role.salary FROM role;",
+    function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      promptUser();
+    }
+  );
+};
+
+function viewAllDepartments() {
+  connection.query(
+    "SELECT name FROM department;", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    promptUser();
+  });
+}
+
+function addEmployee() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the Employee's name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is the Employee's role?",
+      name: "role",
+    },
+  ]);
+}
+
+function addRole() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the new role?",
+      name: "name",
+    },
+  ]);
+}
